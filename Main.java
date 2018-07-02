df76566069e6cc69e563772039ff4d0f5b050956
RemigijusKa <38855837+RemigijusKa@users.noreply.github.com>
2018-07-02 18:28:47 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,20 +7,20 @@ public class Main {
     public static final int HOUR_ARROW_ANGLE_PER_HOUR = 30;
     public static final int MINUTE_ARROW_ANGLE_PER_MINUTE = 6;
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Enter hours (12 or 24 hrs format):");
         int hours = scanner.nextInt();
         hours = hoursConverter(hours);
         System.out.println("Enter minutes: ");
         int minutes = scanner.nextInt();
-        System.out.println("The angle between arrows is " + calculator(hours,minutes) + " degrees.");
+        System.out.println("The angle between arrows is " + calculator(hours, minutes) + " degrees.");
 
         scanner.close();
     }
 
     private static int hoursConverter(int hours) {
-        if (hours > 12){
-            hours = Math.abs(hours - 12);
+        if (hours > 12) {
+            hours = hours - 12;
         }
         return hours;
     }
@@ -30,9 +30,9 @@ public class Main {
         int minuteAngle = minutes * MINUTE_ARROW_ANGLE_PER_MINUTE;
         int angleBetweenArrows = Math.abs(hourAngle - minuteAngle);
 
-        if (angleBetweenArrows > 180){
-            angleBetweenArrows = 360- angleBetweenArrows;
+        if (angleBetweenArrows > 180) {
+            angleBetweenArrows = 360 - angleBetweenArrows;
         }
-        return  angleBetweenArrows;
+        return angleBetweenArrows;
     }
 }
