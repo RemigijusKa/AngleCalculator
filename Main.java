f67b4503a93f3ebe54754f1772f367f8e2503e77
RemigijusKa <38855837+RemigijusKa@users.noreply.github.com>
2018-07-09 14:39:47 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,31 +4,34 @@ public class Main {
 
 
     static Scanner scanner = new Scanner(System.in);
-    public static final int HOUR_ARROW_ANGLE_PER_HOUR = 30;
-    public static final int MINUTE_ARROW_ANGLE_PER_MINUTE = 6;
+    public static final double HOUR_ARROW_ANGLE_PER_HOUR = 30;
+    public static final double HOUR_ARROW_ANGLE_PER_MINUTE = 0.5;
+    public static final double MINUTE_ARROW_ANGLE_PER_MINUTE = 6;
 
     public static void main(String[] args) {
         System.out.println("Enter hours (12 or 24 hrs format):");
-        int hours = scanner.nextInt();
+        double hours = scanner.nextInt();
         hours = hoursConverter(hours);
         System.out.println("Enter minutes: ");
-        int minutes = scanner.nextInt();
+        double minutes = scanner.nextInt();
         System.out.println("The angle between arrows is " + calculator(hours, minutes) + " degrees.");
 
         scanner.close();
     }
 
-    private static int hoursConverter(int hours) {
+    private static double hoursConverter(double hours) {
         if (hours > 12) {
             hours = hours - 12;
         }
         return hours;
     }
 
-    public static int calculator(int hours, int minutes) {
-        int hourAngle = hours * HOUR_ARROW_ANGLE_PER_HOUR;
-        int minuteAngle = minutes * MINUTE_ARROW_ANGLE_PER_MINUTE;
-        int angleBetweenArrows = Math.abs(hourAngle - minuteAngle);
+    public static double calculator(double hours, double minutes) {
+        double hourAngle = hours * HOUR_ARROW_ANGLE_PER_HOUR;
+        hourAngle += minutes * HOUR_ARROW_ANGLE_PER_MINUTE;
+        double minuteAngle = minutes * MINUTE_ARROW_ANGLE_PER_MINUTE;
+
+        double angleBetweenArrows = Math.abs(hourAngle - minuteAngle);
 
         if (angleBetweenArrows > 180) {
             angleBetweenArrows = 360 - angleBetweenArrows;
